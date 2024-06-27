f286ca34ace4a2dcbf367ae2d7d0aec1507b3be2
kiraa123 <kiranprakash970@gmail.com>
2024-06-27 20:27:17 +0530

    frontend signup

--- frontend/src/components/user/Signup.tsx
+++ frontend/src/components/user/Signup.tsx
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+import axios from 'axios'
+import { Link, useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 type SignupForm = {
   email: string;
@@ -7,28 +11,56 @@ type SignupForm = {
 }
 
 const SignupPage = () => {
-  const [formData, setFormData] = useState<SignupForm>({
-    email: '',
-    username: '',
-    password: '',
-  });
+  const navigate= useNavigate();
+  const [email,setEmail]=useState('')
+  const [name,setName]=useState('')
+  const [password,setPassword]= useState('')
+  const [error,setError]= useState('')
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({ ...formData, [event.target.name]: event.target.value });
-  };
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    // Form validation and submission logic here
-    console.log('Email:', formData.email);
-    console.log('Username:', formData.username);
-    console.log('Password:', formData.password);
+  const handleSubmit = async(e: React.FormEvent) => {
+    e.preventDefault();
+    setError('')
+    const namePattern = /^[A-Za-z ]{3,}$/;
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordPattern = /^.{6,}$/;
+
+    if (!name.trim() || !namePattern.test(name)) {
+      toast.error("Name have minimum 3 letters");
+      return;
+  }
+  if (!email.trim() || !emailPattern.test(email)) {
+      toast.error("Invalid email format");
+      return;
+  }
+  if (!password.trim() || !passwordPattern.test(password)) {
+      toast.error("Password have minimum 6 characters");
+      return;
+  }
+    try{
+      const response=axios.post('http://localhost:3000/users/signup',
+        {name,email,password}
+      )
+      console.log(response,'response');
+      if(!response){
+        throw new Error('Failed to signup') 
+      }else{
+        console.log('success');
+        toast.success('Signup successful! Redirecting...');
+        
+        navigate('/email')
+      }
+
+
+    }catch(error){
+      console.log(error);
+    }
   };
 
   return (
     <>
-      <div className="min-h-screen
-       flex flex-col items-center justify-center ">
+    <ToastContainer/>
+      <div className="min-h-screen flex flex-col items-center justify-center ">
         <div className="bg-white p-8 rounded-lg shadow-lg w-96 max-w-lg">
           <h2 className="text-4xl font-bold text-center mb-6 mt-[50px]  " style={{ fontFamily: 'Kavoon, cursive' }}>Anonymous</h2>
           <p className=" text-1xl text-center mb-6 opacity-50">Sign up to see <br /> the mirage world</p>
@@ -55,8 +87,8 @@ const SignupPage = () => {
               <input
                 type="email"
                 name="email"
-                value={formData.email}
-                onChange={handleChange}
+                value={email}
+                onChange={(e)=>setEmail(e.target.value)}
                 placeholder="Email"
                 className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
               />
@@ -64,9 +96,9 @@ const SignupPage = () => {
             <div className="mb-4">
               <input
                 type="text"
-                name="username"
-                value={formData.username}
-                onChange={handleChange}
+                name="name"
+                value={name}
+                onChange={(e)=>setName(e.target.value)}
                 placeholder="Username"
                 className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
               />
@@ -75,8 +107,8 @@ const SignupPage = () => {
               <input
                 type="password"
                 name="password"
-                value={formData.password}
-                onChange={handleChange}
+                value={password}
+                onChange={(e)=>setPassword(e.target.value)}
                 placeholder="Password"
                 className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600"
               />
@@ -93,8 +125,8 @@ const SignupPage = () => {
           </form>
 
         </div> 
-        <div className=" w-full  max-w-md mt-2 h-20 items-center justify-center">
-          <div className="bg-white p-8 rounded-lg shadow-lg h-2 items-center justify-center ">
+        <div className="w-96 max-w-lg mt-2  ">
+          <div className="bg-white p-8 rounded-lg shadow-lg ">
             <p className="text-center mt-[-11px]">
               Have an account?{' '}
               <a href="/login" className="text-blue-600 hover:underline">
